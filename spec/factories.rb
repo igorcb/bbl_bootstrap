bc76c86e7b203bda19c70d4f183e83b8aee9ee96
igorcb <igor.batista@gmail.com>
2013-08-27 23:06:42 -0300

    add factori_girls

---
+++ spec/factories.rb
@@ -0,0 +1,30 @@
+FactoryGirl.define do
+  factory :assunto do
+    descricao "Assunto Exemplo"
+  end
+
+  factory :autor do
+  	descricao "Autor exemplo"
+  	cutter "11.01"
+  end
+
+  factory :casa do
+    descricao "Casa Exemplo"
+  end
+
+  factory :classificacao do
+    cdd "000.00" 
+    descricao "classificacao exemplo"
+  end
+
+  factory :editora do
+    descricao "editora exemplo"
+    cidade "cidade exemplo"
+    ano "1900"
+  end
+
+  factory :local do
+    descricao "Assunto Exemplo"
+  end
+
+end
