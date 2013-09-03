2f953686668ee391eed82f42e8940e2c378e6d56
Igor Batista <igor.batista@gmail.com>
2013-09-02 22:24:21 -0300

    add new user and signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,23 @@ describe "StaticPages" do
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
   
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
 
     before { visit root_path }
+    
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    #it { should have_content('Sample App') }
+    #it { should have_title(full_title('')) }
+    #it { should_not have_title('| Home') }
+
+    it_should_behave_like "all static pages"
     #it { should_not have_title('| Home') }
   end
 
@@ -36,4 +47,6 @@ describe "StaticPages" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  
 end
