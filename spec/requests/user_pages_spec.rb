bae6e6756dbaeb4da23908e45226cc04f2532de4
Igor Batista <igor.batista@gmail.com>
2013-09-04 17:00:34 -0300

    ...

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -82,7 +82,7 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -145,6 +145,7 @@ describe "User pages" do
       end
       before { patch user_path(user), params }
       specify { expect(user.reload).not_to be_admin }
+      
     end
   end
 end
