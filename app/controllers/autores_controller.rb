ee27201e36a382109181feb08e9f4589bff0d85f
igorcb <igor.batista@gmail.com>
2013-09-11 19:42:33 -0300

    finally integration_test autor

--- app/controllers/autores_controller.rb
+++ app/controllers/autores_controller.rb
@@ -3,9 +3,40 @@ class AutoresController < ApplicationController
   before_action :user_admin
 
   def index
-  	# paginate(page: params[:page])
   	@autores = Autor.paginate(page: params[:page])
-  	                
+  end
+
+  def show
+    @autor = Autor.find(params[:id])
+  end
+
+  def new
+    @autor = Autor.new
+  end
+
+  def create
+    @autor = Autor.new(autor_params)    # Not the final implementation!
+    if @autor.save
+      flash[:success] = "Autor was sucessfully created"
+      redirect_to @autor
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @autor = Autor.find(params[:id])
+  end
+
+  def update
+    @autor = Autor.find(params[:id])
+    if @autor.update_attributes(autor_params)
+      flash[:success] = "Autor was sucessfully update."
+      #sign_in @user
+      redirect_to @autor
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -13,5 +44,11 @@ class AutoresController < ApplicationController
     flash[:success] = "Autor destroy sucessfully."
     redirect_to autores_url
   end
+
+  private
+
+    def autor_params
+        params.require(:autor).permit(:descricao, :cutter)     
+    end
 
 end
--- spec/requests/autores_pages_spec.rb
+++ spec/requests/autores_pages_spec.rb
@@ -9,7 +9,6 @@ describe "Autores Paginas" do
   	  before { visit autores_path }
 
   	  it { should have_content('Please sign in.') }
-
     end
 
     describe "como usuario comum " do
@@ -67,9 +66,192 @@ describe "Autores Paginas" do
         end
         # it { should_not have_link('delete', href: usuario_path(admin)) }
       end
+
+      describe "edit links" do
+        let(:admin) { FactoryGirl.create(:admin) }
+         
+        before { @autor = Autor.create(descricao: "Autor Exemplo", cutter: "01.01") }
+        after(:all)  { Autor.delete_all}
+       
+        before do
+          sign_in admin
+          visit autores_path
+        end
     
+        it { should have_link('Editar', href: edit_autor_path(@autor)) }
+      end
+    end
+  end
+
+  describe "show page" do
+  
+    describe "deve estar logado" do
+      let(:autor) { FactoryGirl.create(:autor) }
+      before { visit autor_path(autor) }
+
+      it { should have_content('Please sign in.') }
     end
 
+    describe "como usuario comum " do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      let(:autor) { FactoryGirl.create(:autor) }
+
+      before(:each) do
+        sign_in usuario
+        visit autor_path(autor) 
+      end
+
+      it { should have_content('administradores') }
+    end
+
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+      let(:autor) { FactoryGirl.create(:autor) }
+
+      before do
+        sign_in usuario
+        visit autor_path(autor) 
+      end
+
+      it { should have_content(autor.descricao) }
+      it { should have_content(autor.cutter) }
+      it { should have_title(autor.descricao) }
+    end
+  end
+
+  describe "novo autor" do
+    describe "deve estar logado" do
+      let(:autor) { FactoryGirl.create(:autor) }
+      before { visit new_autor_path  }
+
+      it { should have_content('Please sign in.') }
+    end
+
+    describe "como usuario comum " do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      let(:autor) { FactoryGirl.create(:autor) }
+
+      before(:each) do
+        sign_in usuario
+        visit new_autor_path(autor) 
+      end
+
+      it { should have_content('administradores') }
+    end
+
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+
+      before do
+        sign_in usuario
+        visit new_autor_path 
+      end
+      it { should have_content("Novo autor") }
+      it { should have_title("Novo autor") }
+      it { should have_link("Cancelar", href: autores_path) }
+
+      let(:submit) { "Salvar" }
+
+      describe "com informacoes invalidas" do
+        it "nao deve criar um autor" do
+          expect { click_button submit }.not_to change(Autor, :count)
+        end
+      end
+      
+      describe "com informacoes validas" do
+        let(:novo_autor) { 'Exemplo Autor' }
+        let(:novo_cutter) { '11.91' }
+        before do
+          fill_in "Descricao",         with: novo_autor
+          fill_in "Cutter",            with: novo_cutter
+        end
+
+        it "deve criar um autor" do
+          expect { click_button submit }.to change(Autor, :count).by(1)
+        end
+
+        describe "depois de salvar a autor" do
+          before { click_button submit }
+          let(:autor) { Autor.find_by(descricao: novo_autor, cutter: novo_cutter) }
+
+          #it { should have_link('Sign out') }
+          it { should have_title(autor.descricao) }
+          it { should have_content(autor.descricao) }
+          it { should have_content(autor.cutter) }
+          it { should have_selector('div.alert.alert-success', text: 'sucessfully created') }
+        end
+      end
+
+
+    end
 
   end
+
+  describe "editar" do
+    describe "deve estar logado" do
+      let(:autor) { FactoryGirl.create(:autor) }
+      before { visit edit_autor_path(:autor)  }
+
+      it { should have_content('Please sign in.') }
+    end
+
+    describe "como usuario comum " do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      let(:autor) { FactoryGirl.create(:autor) }
+
+      before(:each) do
+        sign_in usuario
+        visit edit_autor_path(autor) 
+      end
+
+      it { should have_content('administradores') }
+    end
+
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+      let(:autor) { FactoryGirl.create(:autor) }
+      
+      before do
+        sign_in usuario
+        visit edit_autor_path(autor)
+      end
+
+      it { should have_link("Cancelar", href: autores_path) }
+
+      describe "pagina" do
+        it { should have_content('Editar autor') }
+        it { should have_title('Editar autor') }
+      end
+
+      describe "com informacoes invalidas" do
+        before do 
+          fill_in "Descricao",             with: ""
+          fill_in "Cutter",                with: ""
+          click_button "Save changes" 
+        end
+
+        it { should have_content("error") }
+      end
+
+      describe "com informacao vaidas" do
+        let(:new_autor)  { "Novo Autor" }
+        let(:new_cutter)  { "00.01" }
+        before do
+          fill_in "Descricao",             with: new_autor
+          fill_in "Cutter",             with: new_cutter
+          click_button "Save changes"
+        end
+
+        it { should have_title(new_autor) }
+        it { should have_content(new_autor) }
+        it { should have_content(new_cutter) }
+        it { should have_selector('div.alert.alert-success') }
+        #it { should have_link('Sign out', href: signout_path) }
+        specify { expect(autor.reload.descricao).to  eq new_autor }
+        specify { expect(autor.reload.cutter).to  eq new_cutter }
+      end
+    end
+
+  end
+
 end
