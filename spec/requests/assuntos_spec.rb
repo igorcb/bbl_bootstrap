9b1ca6db8dd718fbc81d9147dc5fbd373d9118a2
igorcb <igor.batista@gmail.com>
2013-09-13 00:05:27 -0300

    modify assunto integration test only to admin

--- spec/requests/assuntos_spec.rb
+++ spec/requests/assuntos_spec.rb
@@ -4,168 +4,248 @@ describe "Assuntos pagina" do
   subject { page }
 
   describe "index" do
-  	#let(:usuario) { FactoryGirl.create(:usuario) }
-    let(:usuario) { FactoryGirl.create(:admin) }
+    describe "deve estar logado" do
+      before { visit assuntos_path }
 
-  	before(:each) do
-  		sign_in usuario
-  	  visit assuntos_path
+      it { should have_content('Please sign in.') }
     end
 
-  	it { should have_title('Assuntos') }
-    it { should have_content('Listagem de assuntos') }
-    it { should have_link('Novo Assunto') }
-
-    describe "paginations" do
-      before(:all) { 35.times { FactoryGirl.create(:assunto) } } 
-      after(:all)  { Assunto.delete_all}
-
-      it { should have_selector('div.pagination') }
-
-      it "deve listar cada assunto" do
-       Assunto.paginate(page: 1).each do |assunto|
-         expect(page).to have_selector('td', text: assunto.descricao)
-       end
+    describe "como usuario comum " do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      before(:each) do
+        sign_in usuario
+        visit assuntos_path 
       end
+
+      it { should have_content('administradores') }
     end
 
-    describe "delete links" do
-      it { should_not have_link('delete') }
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
 
-      describe "como um usuario administrador" do
-        before { Assunto.create(descricao: "Exemplo teste") }
+      before(:each) do
+        sign_in usuario
+        visit assuntos_path
+      end
+
+      it { should have_title('Assuntos') }
+      it { should have_content('Listagem de assuntos') }
+      it { should have_link('Novo Assunto') }
+
+      describe "paginations" do
+        before(:all) { 35.times { FactoryGirl.create(:assunto) } } 
+        after(:all)  { Assunto.delete_all}
+
+        it { should have_selector('div.pagination') }
+
+        it "deve listar cada assunto" do
+         Assunto.paginate(page: 1).each do |assunto|
+           expect(page).to have_selector('td', text: assunto.descricao)
+         end
+        end
+      end
+
+      describe "delete links" do
+        it { should_not have_link('delete') }
+
+        describe "como um usuario administrador" do
+          before { Assunto.create(descricao: "Exemplo teste") }
+
+          let(:admin) { FactoryGirl.create(:admin) }
+          
+          before do
+            sign_in admin
+            visit assuntos_path
+          end
+        
+          it { should have_link('Delete', href: assunto_path(Assunto.first)) }
+
+          it "deve ser capaz de apagar assunto" do
+            expect do
+              click_link('Delete', match: :first)
+            end.to change(Assunto, :count).by(-1)
+          end
+          # it { should_not have_link('delete', href: usuario_path(admin)) }
+        end
+      end
+
+      describe "edit links" do
+        before { @assunto = Assunto.create(descricao: "Exemplo teste") }
+        after(:all)  { Assunto.delete_all}
 
         let(:admin) { FactoryGirl.create(:admin) }
         
         before do
           sign_in admin
-       	  visit assuntos_path
+          visit assuntos_path
         end
       
-        it { should have_link('Delete', href: assunto_path(Assunto.first)) }
-
-        it "deve ser capaz de apagar assunto" do
-          expect do
-            click_link('Delete', match: :first)
-          end.to change(Assunto, :count).by(-1)
-        end
-        # it { should_not have_link('delete', href: usuario_path(admin)) }
+        it { should have_link('Editar', href: edit_assunto_path(@assunto)) }
       end
-    end
 
-    describe "edit links" do
-      before { @assunto = Assunto.create(descricao: "Exemplo teste") }
-      after(:all)  { Assunto.delete_all}
-
-      let(:admin) { FactoryGirl.create(:admin) }
-      
-      before do
-        sign_in admin
-     	  visit assuntos_path
-      end
-    
-      it { should have_link('Editar', href: edit_assunto_path(@assunto)) }
     end
   end
 
   describe "show page" do
-    let(:usuario) { FactoryGirl.create(:admin) }
-    let(:assunto) { FactoryGirl.create(:assunto) }
-    
-    before do
-      sign_in usuario
-      visit assunto_path(assunto) 
+    describe "deve estar logado" do
+      let(:assunto) { FactoryGirl.create(:assunto) }
+      before { visit assunto_path(assunto) }
+
+      it { should have_content('Please sign in.') }
     end
 
-    it { should have_content(assunto.descricao) }
-    it { should have_title(assunto.descricao) }
+    describe "como usuario comum " do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      let(:assunto) { FactoryGirl.create(:assunto) }
+
+      before(:each) do
+        sign_in usuario
+        visit assunto_path(assunto) 
+      end
+
+      it { should have_content('administradores') }
+    end
+
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+      let(:assunto) { FactoryGirl.create(:assunto) }
+      
+      before do
+        sign_in usuario
+        visit assunto_path(assunto) 
+      end
+
+      it { should have_content(assunto.descricao) }
+      it { should have_title(assunto.descricao) }
+    end
   end
 
   describe "novo assunto" do
-    let(:usuario) { FactoryGirl.create(:admin) }
+    describe "deve estar logado" do
+      let(:assunto) { FactoryGirl.create(:assunto) }
+      before { visit assunto_path(assunto) }
 
-    before do
-      sign_in usuario
-      visit new_assunto_path 
+      it { should have_content('Please sign in.') }
     end
-    it { should have_link("Cancelar", href: assuntos_path) }
 
-    let(:submit) { "Salvar" }
+    describe "como usuario comum " do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      let(:assunto) { FactoryGirl.create(:assunto) }
 
-    describe "com informacoes invalidas" do
-      it "nao deve criar um assunto" do
-        expect { click_button submit }.not_to change(Assunto, :count)
+      before(:each) do
+        sign_in usuario
+        visit assunto_path(assunto) 
       end
+
+      it { should have_content('administradores') }
     end
-    
-    describe "com informacoes validas" do
-    	let(:nova_descricao) { 'Assunto exemplo' }
+
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+
       before do
-        fill_in "Descricao",         with: nova_descricao
+        sign_in usuario
+        visit new_assunto_path 
       end
+      it { should have_link("Cancelar", href: assuntos_path) }
 
-      it "deve criar um assunto" do
-        expect { click_button submit }.to change(Assunto, :count).by(1)
+      let(:submit) { "Salvar" }
+
+      describe "com informacoes invalidas" do
+        it "nao deve criar um assunto" do
+          expect { click_button submit }.not_to change(Assunto, :count)
+        end
       end
+      
+      describe "com informacoes validas" do
+      	let(:nova_descricao) { 'Assunto exemplo' }
+        before do
+          fill_in "Descricao",         with: nova_descricao
+        end
 
-      describe "depois de salvar o usuario" do
-        before { click_button submit }
-        let(:assunto) { Assunto.find_by(descricao: nova_descricao) }
+        it "deve criar um assunto" do
+          expect { click_button submit }.to change(Assunto, :count).by(1)
+        end
 
-        #it { should have_link('Sign out') }
-        it { should have_title(assunto.descricao) }
-        it { should have_selector('div.alert.alert-success', text: 'sucessfully created') }
+        describe "depois de salvar o usuario" do
+          before { click_button submit }
+          let(:assunto) { Assunto.find_by(descricao: nova_descricao) }
+
+          #it { should have_link('Sign out') }
+          it { should have_title(assunto.descricao) }
+          it { should have_selector('div.alert.alert-success', text: 'sucessfully created') }
+        end
       end
     end
   end
 
   describe "editar" do
-    let(:usuario) { FactoryGirl.create(:admin) }
-    let(:assunto) { FactoryGirl.create(:assunto) }
-    
-    before do
-      sign_in usuario
-      visit edit_assunto_path(assunto)
+    describe "deve estar logado" do
+      let(:assunto) { FactoryGirl.create(:assunto) }
+      before { visit assunto_path(assunto) }
+
+      it { should have_content('Please sign in.') }
     end
 
-    it { should have_link("Cancelar", href: assuntos_path) }
+    describe "como usuario comum " do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      let(:assunto) { FactoryGirl.create(:assunto) }
 
-    describe "pagina" do
-      it { should have_content('Editar assunto') }
-      it { should have_title('Editar assunto') }
-      #it { should have_link('change', href: 'http://gravatar.com/emails') }
-    end
-
-    describe "com informacoes invalidas" do
-      before do 
-        fill_in "Descricao",             with: ""
-      	click_button "Save changes" 
+      before(:each) do
+        sign_in usuario
+        visit assunto_path(assunto) 
       end
 
-      it { should have_content("error") }
+      it { should have_content('administradores') }
     end
 
-    describe "com informacao vaidas" do
-      let(:new_name)  { "New Name" }
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+      let(:assunto) { FactoryGirl.create(:assunto) }
+      
       before do
-        fill_in "Descricao",             with: new_name
-        click_button "Save changes"
+        sign_in usuario
+        visit edit_assunto_path(assunto)
       end
 
-      it { should have_title(new_name) }
-      it { should have_selector('div.alert.alert-success') }
-      #it { should have_link('Sign out', href: signout_path) }
-      specify { expect(assunto.reload.descricao).to  eq new_name }
+      it { should have_link("Cancelar", href: assuntos_path) }
+
+      describe "pagina" do
+        it { should have_content('Editar assunto') }
+        it { should have_title('Editar assunto') }
+        #it { should have_link('change', href: 'http://gravatar.com/emails') }
+      end
+
+      describe "com informacoes invalidas" do
+        before do 
+          fill_in "Descricao",             with: ""
+        	click_button "Save changes" 
+        end
+
+        it { should have_content("error") }
+      end
+
+      describe "com informacao vaidas" do
+        let(:new_name)  { "New Name" }
+        before do
+          fill_in "Descricao",             with: new_name
+          click_button "Save changes"
+        end
+
+        it { should have_title(new_name) }
+        it { should have_selector('div.alert.alert-success') }
+        #it { should have_link('Sign out', href: signout_path) }
+        specify { expect(assunto.reload.descricao).to  eq new_name }
+      end
+      
+      # describe "forbidden attributes" do
+      #   let(:params) do
+      #     { assunto: { admin: true, password: usuario.password,
+      #               password_confirmation: usuario.password } }
+      #   end
+      #   before { patch usuario_path(usuario), params }
+      #   specify { expect(usuario.reload).not_to be_admin }
+      # end
     end
-    
-    # describe "forbidden attributes" do
-    #   let(:params) do
-    #     { assunto: { admin: true, password: usuario.password,
-    #               password_confirmation: usuario.password } }
-    #   end
-    #   before { patch usuario_path(usuario), params }
-    #   specify { expect(usuario.reload).not_to be_admin }
-    # end
- 
   end
 end
