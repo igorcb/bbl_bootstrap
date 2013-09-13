4c39e4f7e9a39624793d30ec15bd46c2fb102b2c
igorcb <igor.batista@gmail.com>
2013-09-13 00:35:01 -0300

    modify casas integration test to admin

--- spec/requests/casas_pages_spec.rb
+++ spec/requests/casas_pages_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe "Casas pagina" do
   subject { page }
 
-
   describe "index" do
+    describe "deve estar logado" do
+      before { visit casas_path }
+
+      it { should have_content("Please sign in.") }
+    end
+
     describe "como um usuario comum" do
       let(:usuario) { FactoryGirl.create(:usuario) }
       before(:each) do
@@ -15,33 +20,33 @@ describe "Casas pagina" do
       it { should have_content('administradores') }
     end
 
-	  let(:usuario) { FactoryGirl.create(:admin) }
-   	
-   	before(:each) do
-  	  sign_in usuario
-  	  visit casas_path
-    end
-  	it { should have_title('Casas') }
-    it { should have_content('Listagem de casas') }
-    it { should have_link('Nova Casa') }
-
-    describe "paginations" do
-      before(:all) { 35.times { FactoryGirl.create(:casa) } } 
-      after(:all)  { Casa.delete_all}
-
-      it { should have_selector('div.pagination') }
-
-      it "deve listar cada casa" do
-       Casa.paginate(page: 1).each do |casa|
-         expect(page).to have_selector('td', text: casa.descricao)
-       end
+    describe "como usuario administrador" do
+  	  let(:usuario) { FactoryGirl.create(:admin) }
+     	
+     	before(:each) do
+    	  sign_in usuario
+    	  visit casas_path
       end
-    end
+    	it { should have_title('Casas') }
+      it { should have_content('Listagem de casas') }
+      it { should have_link('Nova casa') }
 
-    describe "delete links" do
-      it { should_not have_link('delete') }
+      describe "paginations" do
+        before(:all) { 35.times { FactoryGirl.create(:casa) } } 
+        after(:all)  { Casa.delete_all}
 
-      describe "como um usuario administrador" do
+        it { should have_selector('div.pagination') }
+
+        it "deve listar cada casa" do
+         Casa.paginate(page: 1).each do |casa|
+           expect(page).to have_selector('td', text: casa.descricao)
+         end
+        end
+      end
+
+      describe "delete links" do
+        it { should_not have_link('delete') }
+
         before { Casa.create(descricao: "Exemplo teste") }
 
         let(:admin) { FactoryGirl.create(:admin) }
@@ -60,115 +65,176 @@ describe "Casas pagina" do
         end
         # it { should_not have_link('delete', href: usuario_path(admin)) }
       end
-    end
 
-    describe "edit links" do
-      before { @casa = Casa.create(descricao: "Exemplo teste") }
-      after(:all)  { Casa.delete_all}
+      describe "edit links" do
+        before { @casa = Casa.create(descricao: "Exemplo teste") }
+        after(:all)  { Casa.delete_all}
 
-      let(:admin) { FactoryGirl.create(:admin) }
+        let(:admin) { FactoryGirl.create(:admin) }
+        
+        before do
+          sign_in admin
+          visit casas_path
+        end
       
-      before do
-        sign_in admin
-        visit casas_path
+        it { should have_link('Editar', href: edit_casa_path(@casa)) }
       end
-    
-      it { should have_link('Editar', href: edit_casa_path(@casa)) }
     end
+  end
 
   describe "show page" do
-    let(:usuario) { FactoryGirl.create(:admin) }
+
+    describe "deve estar logado" do
     let(:casa) { FactoryGirl.create(:casa) }
-    
-    before do
-      sign_in usuario
-      visit casa_path(casa) 
+      before { visit casas_path(casa) }
+
+      it { should have_content("Please sign in.") }
     end
 
-    it { should have_content(casa.descricao) }
-    it { should have_title(casa.descricao) }
+    describe "como um usuario comum" do
+      let(:casa) { FactoryGirl.create(:casa) }
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      before(:each) do
+        sign_in usuario
+        visit casas_path(casa)
+      end
+ 
+      it { should have_content('administradores') }
+    end
+
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+      let(:casa) { FactoryGirl.create(:casa) }
+      
+      before do
+        sign_in usuario
+        visit casa_path(casa) 
+      end
+
+      it { should have_content(casa.descricao) }
+      it { should have_title(casa.descricao) }
+    end
   end
 
   describe "nova casa" do
-    let(:usuario) { FactoryGirl.create(:admin) }
+    describe "deve estar logado" do
 
-    before do
-      sign_in usuario
-      visit new_casa_path 
+      before { visit new_casa_path }
+
+      it { should have_content("Please sign in.") }
     end
-    it { should have_content("Nova casa") }
-    it { should have_title("Nova casa") }
-    it { should have_link("Cancelar", href: casas_path) }
 
-    let(:submit) { "Salvar" }
-
-    describe "com informacoes invalidas" do
-      it "nao deve criar uma casa" do
-        expect { click_button submit }.not_to change(Casa, :count)
+    describe "como um usuario comum" do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+  
+      before(:each) do
+        sign_in usuario
+        visit new_casa_path
       end
+ 
+      it { should have_content('administradores') }
     end
-    
-    describe "com informacoes validas" do
-      let(:nova_descricao) { 'Casa exemplo' }
+  
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+
       before do
-        fill_in "Descricao",         with: nova_descricao
+        sign_in usuario
+        visit new_casa_path 
       end
+      it { should have_content("Nova casa") }
+      it { should have_title("Nova casa") }
+      it { should have_link("Cancelar", href: casas_path) }
 
-      it "deve criar um casa" do
-        expect { click_button submit }.to change(Casa, :count).by(1)
+      let(:submit) { "Salvar" }
+
+      describe "com informacoes invalidas" do
+        it "nao deve criar uma casa" do
+          expect { click_button submit }.not_to change(Casa, :count)
+        end
       end
+      
+      describe "com informacoes validas" do
+        let(:nova_descricao) { 'Casa exemplo' }
+        before do
+          fill_in "Descricao",         with: nova_descricao
+        end
 
-      describe "depois de salvar a casa" do
-        before { click_button submit }
-        let(:casa) { Casa.find_by(descricao: nova_descricao) }
+        it "deve criar um casa" do
+          expect { click_button submit }.to change(Casa, :count).by(1)
+        end
 
-        #it { should have_link('Sign out') }
-        it { should have_title(casa.descricao) }
-        it { should have_selector('div.alert.alert-success', text: 'sucessfully created') }
+        describe "depois de salvar a casa" do
+          before { click_button submit }
+          let(:casa) { Casa.find_by(descricao: nova_descricao) }
+
+          #it { should have_link('Sign out') }
+          it { should have_title(casa.descricao) }
+          it { should have_selector('div.alert.alert-success', text: 'sucessfully created') }
+        end
       end
     end
   end
 
   describe "editar" do
-    let(:usuario) { FactoryGirl.create(:admin) }
-    let(:casa) { FactoryGirl.create(:casa) }
-    
-    before do
-      sign_in usuario
-      visit edit_casa_path(casa)
+    describe "deve estar logado" do
+      let(:casa) { FactoryGirl.create(:casa) }
+      before { visit edit_casa_path(casa) }
+
+      it { should have_content("Please sign in.") }
     end
 
-    it { should have_link("Cancelar", href: casas_path) }
-
-    describe "pagina" do
-      it { should have_content('Editar casa') }
-      it { should have_title('Editar casa') }
-    end
-
-    describe "com informacoes invalidas" do
-      before do 
-        fill_in "Descricao",             with: ""
-        click_button "Save changes" 
+    describe "como um usuario comum" do
+      let(:casa) { FactoryGirl.create(:casa) }
+      let(:usuario) { FactoryGirl.create(:usuario) }
+  
+      before(:each) do
+        sign_in usuario
+        visit edit_casa_path(casa)
       end
-
-      it { should have_content("error") }
+ 
+      it { should have_content('administradores') }
     end
-
-    describe "com informacao vaidas" do
-      let(:new_name)  { "New Name" }
+  
+    describe "como usuario administrador" do
+      let(:usuario) { FactoryGirl.create(:admin) }
+      let(:casa) { FactoryGirl.create(:casa) }
+      
       before do
-        fill_in "Descricao",             with: new_name
-        click_button "Save changes"
+        sign_in usuario
+        visit edit_casa_path(casa)
       end
 
-      it { should have_title(new_name) }
-      it { should have_selector('div.alert.alert-success') }
-      #it { should have_link('Sign out', href: signout_path) }
-      specify { expect(casa.reload.descricao).to  eq new_name }
+      it { should have_link("Cancelar", href: casas_path) }
+
+      describe "pagina" do
+        it { should have_content('Editar casa') }
+        it { should have_title('Editar casa') }
+      end
+
+      describe "com informacoes invalidas" do
+        before do 
+          fill_in "Descricao",             with: ""
+          click_button "Save changes" 
+        end
+
+        it { should have_content("error") }
+      end
+
+      describe "com informacao vaidas" do
+        let(:new_name)  { "New Name" }
+        before do
+          fill_in "Descricao",             with: new_name
+          click_button "Save changes"
+        end
+
+        it { should have_title(new_name) }
+        it { should have_selector('div.alert.alert-success') }
+        #it { should have_link('Sign out', href: signout_path) }
+        specify { expect(casa.reload.descricao).to  eq new_name }
+      end
     end
 
   end
-
-end
 
 end
